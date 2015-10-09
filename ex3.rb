50ab15c7eb3a83ff9c42ca308c1de6ac10564969
arcetera <arcticsn0wdev@gmail.com>
2015-10-08 21:47:34 -0400

    why

---
+++ ex3.rb
@@ -0,0 +1,37 @@
+# prints text for context
+puts "I will now count my chickens:"
+
+# calculates 25 + 30 / 6 and then prints it as the number of hens
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# calculates 100 - 25 * 3 % 4 and prints it as the number of rootsters
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# prints text for context
+puts "Now I will count the eggs:"
+
+# does complex calculation and prints result
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# prints text for contest
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# calculates each value and compares them using the operator, then checks whether the operator is correct
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# prints text for context and calculates an expression
+puts "What is 3 + 2? #{3.0 + 2.0}"
+# prints text for context and calculates an expression
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# prints text for context
+puts "Oh, that's why it's false."
+
+# prints text for context
+puts "How about some more."
+
+# prints text and compares values with operator
+puts "Is it greater? #{5.0 > -2.0}"
+# prints text and compares values
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# prints text and compares values
+puts "Is it less or equal? #{5.0 <= -2.0}"
