df1ecfbae4533fef202589a30d3d91bd993ce07d
arcetera <arcticsn0wdev@gmail.com>
2015-10-10 10:43:13 -0400

    why am I DOING THIS why

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+height_cm = 2.54 * height
+weight = 180 # lbs
+weight_kg = 0.453592 * weight
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall, and #{height_cm} in centimeters."
+puts "He's #{weight} pounds heavy, and #{weight_kg} in kilograms."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
